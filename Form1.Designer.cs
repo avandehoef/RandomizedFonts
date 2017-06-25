aae0a6fd44f751c51c4588a783ebea00b7353041
Adriaan <avandehoef@outlook.com>
2017-06-25 13:43:10 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Chaos
+{
+    partial class FormFonts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxInput = new System.Windows.Forms.RichTextBox();
+            this.btnRandomize = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.textBoxWelcome = new System.Windows.Forms.RichTextBox();
+            this.btnRecover = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxInput.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxInput.Location = new System.Drawing.Point(51, 87);
+            this.textBoxInput.MaxLength = 150000;
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.textBoxInput.Size = new System.Drawing.Size(804, 610);
+            this.textBoxInput.TabIndex = 0;
+            this.textBoxInput.Text = "";
+            // 
+            // btnRandomize
+            // 
+            this.btnRandomize.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRandomize.Location = new System.Drawing.Point(51, 13);
+            this.btnRandomize.Name = "btnRandomize";
+            this.btnRandomize.Size = new System.Drawing.Size(136, 68);
+            this.btnRandomize.TabIndex = 1;
+            this.btnRandomize.Text = "Verander Lettertype";
+            this.btnRandomize.UseVisualStyleBackColor = true;
+            this.btnRandomize.Click += new System.EventHandler(this.btnRandomize_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.Maroon;
+            this.btnClose.Location = new System.Drawing.Point(719, 703);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(136, 68);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Afsluiten";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.Red;
+            this.btnReset.Location = new System.Drawing.Point(571, 703);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(136, 68);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Clear";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // textBoxWelcome
+            // 
+            this.textBoxWelcome.BackColor = System.Drawing.SystemColors.Control;
+            this.textBoxWelcome.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxWelcome.Enabled = false;
+            this.textBoxWelcome.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxWelcome.ForeColor = System.Drawing.Color.Black;
+            this.textBoxWelcome.Location = new System.Drawing.Point(208, 13);
+            this.textBoxWelcome.MaxLength = 120;
+            this.textBoxWelcome.Name = "textBoxWelcome";
+            this.textBoxWelcome.Size = new System.Drawing.Size(647, 68);
+            this.textBoxWelcome.TabIndex = 4;
+            this.textBoxWelcome.Text = "Geef hieronder een tekst in en klik hiernaast op \"Verander Lettertype\" om de teks" +
+    "t aan te passen";
+            // 
+            // btnRecover
+            // 
+            this.btnRecover.Font = new System.Drawing.Font("Calibri", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRecover.Location = new System.Drawing.Point(51, 703);
+            this.btnRecover.Name = "btnRecover";
+            this.btnRecover.Size = new System.Drawing.Size(136, 68);
+            this.btnRecover.TabIndex = 5;
+            this.btnRecover.Text = "Herstel Lettertype";
+            this.btnRecover.UseVisualStyleBackColor = true;
+            this.btnRecover.Click += new System.EventHandler(this.btnRecover_Click);
+            // 
+            // FormFonts
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(908, 800);
+            this.Controls.Add(this.btnRecover);
+            this.Controls.Add(this.textBoxWelcome);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnRandomize);
+            this.Controls.Add(this.textBoxInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormFonts";
+            this.Opacity = 0.98D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Randomize Fonts";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox textBoxInput;
+        private System.Windows.Forms.Button btnRandomize;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.RichTextBox textBoxWelcome;
+        private System.Windows.Forms.Button btnRecover;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Chaos
+{
+    public partial class FormFonts : Form
+    {
+        public FormFonts()
+        {
+            InitializeComponent();
+        }
+
+        string defaultFont = "Times New Roman";
+        string newFont;
+        int defaultFontSize = 12;
+        Random random = new Random();
+        int randomFontNumber, positie;
+        List<string> letterTypen = new List<string>();
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            textBoxInput.Text = "";
+        }
+
+        private void btnRecover_Click(object sender, EventArgs e)
+        {
+            textBoxInput.Font = new Font(defaultFont, defaultFontSize);
+        }
+
+        private void btnRandomize_Click(object sender, EventArgs e)
+        {
+            randomFontNumber = random.Next(1, 297);
+
+            try
+            {
+                letterTypen = File.ReadAllLines(@"lettertypen.txt").ToList();
+            }
+            catch
+            {
+                textBoxInput.Text = "fout";
+            }
+
+            for (positie = 0; positie < textBoxInput.TextLength; positie = positie + 1)
+            {
+                var newFontTemp = letterTypen.ElementAt(randomFontNumber);
+                newFont = Convert.ToString(newFontTemp);
+                textBoxInput.SelectionStart = positie;
+                textBoxInput.SelectionLength = 1;
+                textBoxInput.SelectionFont = new Font(newFont, defaultFontSize);
+                randomFontNumber = random.Next(1, 297);
+            }
+        }
+    }
+}
